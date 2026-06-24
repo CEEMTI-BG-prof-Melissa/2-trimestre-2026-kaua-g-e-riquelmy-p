ee674c3a47b709eaad1c17c90fe3a11c53e84a5c
Ferreira01 <kaua.gflugao@aluno.edu.es.gov.br>
2026-06-24 12:00:28 -0300

    Aula 02 PHP

---
+++ Aula-02-PHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Aula PHP</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+        $idade=16;
+        $Idade=16;
+        $nome="Kauã";
+        $Nome="Riquelmy";
+
+        echo "Meu nome é ". $nome. " e eu tenho ". $idade. " anos! ". "Minha dupla se chama ". $Nome. " e ele tem ". $Idade. " anos!";
+        
+    ?>
+</body>
+</html>
